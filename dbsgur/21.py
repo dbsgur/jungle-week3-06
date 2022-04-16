63dfd3cc34fe6bc4c418ed94cddc4723b6a0fa67
dbsgur <kkr431@naver.com>
2022-04-16 20:35:16 +0900

    [dbsgur] dd

--- dbsgur/21.py
+++ dbsgur/21.py
@@ -9,7 +9,13 @@ N = int(input())
 
 M = int(input())
 
-realations = []
+realations = [[0] * (N+1) for _ in range(N+1)]
 
 for _ in range(M):
+    # X : 완제품 / Y : 필요 제품 / K : 필요한 갯수
     X, Y, K = map(int, input().split())
+    realations[X][Y] = K
+
+indegree = [0] * (N+1)
+
+dq = deque()
